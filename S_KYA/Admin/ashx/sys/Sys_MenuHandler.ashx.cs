a3f01d9ae77540010b4b41c027cfc08830899dd7
lapShu <295219383@qq.com>
2018-07-13 11:50:01 +0800

    菜单可修改到数据库

--- S_KYA/Admin/ashx/sys/Sys_MenuHandler.ashx.cs
+++ S_KYA/Admin/ashx/sys/Sys_MenuHandler.ashx.cs
@@ -24,6 +24,8 @@ namespace S_KYA.Admin.ashx.sys
                     context.Response.End();
                     break;
                 case "Update":
+                    EditSys_Menue();
+                    context.Response.End();
                     break;
                 case "Delete":
                     break;
@@ -34,6 +36,9 @@ namespace S_KYA.Admin.ashx.sys
             }
 
         }
+        /// <summary>
+        /// 新增菜单
+        /// </summary>
         public void AddSys_Menue()
         {
             Mod_Sys_Menu _Sys_Menu = new Mod_Sys_Menu();
@@ -52,6 +57,28 @@ namespace S_KYA.Admin.ashx.sys
             }
             HttpContext.Current.Response.Write(JSONhelper.ToJson(mod_Com_Json));
         }
+        /// <summary>
+        /// 修改菜单
+        /// </summary>
+        public void EditSys_Menue()
+        {
+            Mod_Sys_Menu _Sys_Menu = new Mod_Sys_Menu();
+            BindDataToModel(ref _Sys_Menu);
+            bool rval = Bll_Sys_Menu.Instance.Update(_Sys_Menu);
+            Mod_Com_Json mod_Com_Json = new Mod_Com_Json();
+            if (rval)
+            {
+                mod_Com_Json.Message = "修改成功";
+                mod_Com_Json.StatuCode = "200";
+            }
+            else
+            {
+                mod_Com_Json.Message = "修改失败";
+                mod_Com_Json.StatuCode = "-1";
+            }
+            HttpContext.Current.Response.Write(JSONhelper.ToJson(mod_Com_Json));
+        }
+
 
         public void BindDataToModel(ref Mod_Sys_Menu _Sys_Menu)
         {
@@ -61,7 +88,7 @@ namespace S_KYA.Admin.ashx.sys
             _Sys_Menu.Icon = HttpContext.Current.Request.Form["pmenuIcon"];
             _Sys_Menu.Pid = Convert.ToInt32(HttpContext.Current.Request.Form["pmenuPid"]);
             _Sys_Menu.Icon = HttpContext.Current.Request.Form["Icon"];
-            _Sys_Menu.Sort = Convert.ToInt32(HttpContext.Current.Request.Form["pmenuSort"]);
+            _Sys_Menu.Sort =HttpContext.Current.Request.Form["pmenuSort"]==""?0 : Convert.ToInt32(HttpContext.Current.Request.Form["pmenuSort"]);
             if (Operate != "Insert")
             {
                 _Sys_Menu.MenuId = Convert.ToInt32(HttpContext.Current.Request["pmenuId"]);
